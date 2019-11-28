8757fb2b3ac49dbc592a22d4fe7b9edc6be895a5
Konrad Merz <konrad@graylog.com>
2019-11-28 10:40:42 +0100

    Add shapes to show alerts in widgets

--- graylog2-web-interface/src/views/components/visualizations/line/LineVisualization.jsx
+++ graylog2-web-interface/src/views/components/visualizations/line/LineVisualization.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import { AggregationType } from 'views/components/aggregationbuilder/AggregationBuilderPropTypes';
 import type { VisualizationComponent, VisualizationComponentProps } from 'views/components/aggregationbuilder/AggregationBuilder';
+import EventHandler from 'views/logic/searchtypes/events/EventHandler';
 import type { ChartDefinition } from '../ChartData';
 
 import { chartData } from '../ChartData';
@@ -22,13 +23,23 @@ const getChartColor = (fullData, name) => {
 
 const setChartColor = (chart, colors) => ({ line: { color: colors[chart.name] } });
 
-const LineVisualization: VisualizationComponent = ({ config, data, effectiveTimerange }: VisualizationComponentProps) => (
-  <XYPlot config={config}
-          effectiveTimerange={effectiveTimerange}
-          getChartColor={getChartColor}
-          setChartColor={setChartColor}
-          chartData={chartData(config, data.chart || Object.values(data)[0], 'scatter', chartGenerator)} />
-);
+const LineVisualization: VisualizationComponent = ({ config, data, effectiveTimerange }: VisualizationComponentProps) => {
+  const chartDataResult = chartData(config, data.chart || Object.values(data)[0], 'scatter', chartGenerator);
+  const layout = {};
+  if (config.eventAnnotation && data.events) {
+    const { eventChartData, shapes } = EventHandler.toVisualizationData(data.events, config.formattingSettings);
+    chartDataResult.push(eventChartData);
+    layout.shapes = shapes;
+  }
+  return (
+    <XYPlot config={config}
+            effectiveTimerange={effectiveTimerange}
+            getChartColor={getChartColor}
+            setChartColor={setChartColor}
+            plotLayout={layout}
+            chartData={chartDataResult} />
+  );
+};
 
 LineVisualization.propTypes = {
   config: AggregationType.isRequired,
